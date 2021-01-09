9f485d125fa63fc4e4f6026967bbefb67ea6a49a
Maik Möwes <moewes@users.noreply.github.com>
2021-01-09 12:38:00 +0100

    Ui5Link missing properties and JavaDoc

--- src/main/java/net/moewes/cloud/ui/ui5/Ui5Link.java
+++ src/main/java/net/moewes/cloud/ui/ui5/Ui5Link.java
@@ -4,28 +4,56 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.moewes.cloud.ui.UiComponent;
 
-// TODO JavaDoc
+/**
+ * Java wrapper for UI5 web component ui5-link
+ */
 public class Ui5Link extends UiComponent {
 
+    /**
+     * default constructor
+     */
     public Ui5Link() {
         super("ui5-link");
     }
 
+    /**
+     * constructor
+     *
+     * @param text the text to display
+     * @param url  url to open when the link is clicked
+     */
     public Ui5Link(String text, String url) {
         this();
         setText(text);
         setHref(url);
     }
 
+    /**
+     * sets the link text
+     *
+     * @param text the text to display
+     */
     public void setText(String text) {
         getElement().setInnerHtml(text);
     }
 
+    /**
+     * sets the href attribute of ui5-link
+     *
+     * @param url url to open when the link is clicked
+     */
     public void setHref(String url) {
         getElement().setAttribute("href", url);
     }
 
-    // TODO target
+    /**
+     * sets the target attribute of ui5-link
+     *
+     * @param taget taget of link - see HTML docs for meaning
+     */
+    public void setTarget(Target taget) {
+        getElement().setAttribute("target", taget.getText());
+    }
 
     public void setDisabled(boolean disabled) {
         if (disabled) {
@@ -33,17 +61,49 @@ public class Ui5Link extends UiComponent {
         }
     }
 
+    /**
+     * sets the design attribute of ui5-link
+     *
+     * @param design design
+     */
     public void setDesign(Design design) {
         getElement().setAttribute("design", design.getText());
     }
 
-    // TODO wrap
+    /**
+     * sets the wrap attribute of ui5-link
+     *
+     * @param wrap true when the link text should wrap if there is not enough space
+     */
+    public void setWrap(boolean wrap) {
+        if (wrap) {
+            getElement().setAttribute("wrap", "true");
+        }
+    }
 
+    /**
+     * possible values for the design attribute of ui5-link
+     */
     @AllArgsConstructor
     public enum Design {
         DEFAULT("Default"),
         SUBTLE("Subtle"),
         EMPHASIZED("Emphasized");
+
+        @Getter
+        private final String text;
+    }
+
+    /**
+     * possible values for target attribute of ui5-link
+     */
+    @AllArgsConstructor
+    public enum Target {
+        SELF("_self"),
+        TOP("_top"),
+        BLANK("_black"),
+        PARENT("_parent"),
+        SEARCH("_search");
 
         @Getter
         private final String text;
