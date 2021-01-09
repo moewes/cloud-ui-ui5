0177d102a4a24f71b93276d73985cff8f74e4221
Maik Möwes <moewes@users.noreply.github.com>
2021-01-09 14:14:05 +0100

    ui5-avatar

--- src/main/java/net/moewes/cloud/ui/ui5/Ui5Avatar.java
+++ src/main/java/net/moewes/cloud/ui/ui5/Ui5Avatar.java
@@ -5,48 +5,111 @@ import lombok.Getter;
 import net.moewes.cloud.ui.UiComponent;
 
 /**
- * Java Wrapper for ui5 web component avatar
+ * Java wrapper for ui5 web component avatar
  */
 public class Ui5Avatar extends UiComponent {
 
+    /**
+     * default constructor
+     */
     public Ui5Avatar() {
         super("ui5-avatar");
     }
 
-    public void setImage(String image) {
-        getElement().setAttribute("image", image);
+    /**
+     * sets a image to display
+     *
+     * @param imageUrl image url
+     */
+    public void setImage(String imageUrl) {
+        getElement().setAttribute("image", imageUrl);
     }
 
     public void setShape(Shape shape) {
         getElement().setAttribute("shape", shape.getAttribute());
     }
 
+    /**
+     * sets the size attribute of ui5-avatar
+     *
+     * @param size size
+     */
     public void setSize(Size size) {
         getElement().setAttribute("size", size.name());
     }
 
-    public void setIcon(String icon) { // TODO Icon.Enum
-        getElement().setAttribute("icon", icon);
+    /**
+     * sets the icon. an icon is not displayd if an image url is set
+     *
+     * @param iconName icon name - see ui5 icon explorer
+     */
+    public void setIcon(String iconName) {
+        getElement().setAttribute("icon", iconName);
     }
 
+    /**
+     * sets the initials attribute of ui5-avatar
+     *
+     * @param initials up to two latin letters are displayed as initials
+     */
     public void setInitials(String initials) {
         getElement().setAttribute("initials", initials);
     }
 
-    // TODO accessible Name
-    // TODO background Color
-    // TODO image fit type
-    // TODO interactive
+    /**
+     * sets the accessible-name attribute of ui5-avatar
+     *
+     * @param name name for aria
+     */
+    public void setAccessibleName(String name) {
+        getElement().setAttribute("accessible-name", name);
+    }
 
+    /**
+     * sets the interactive attribute of ui5-avatar
+     *
+     * @param interactive flag
+     */
+    public void setInteractive(boolean interactive) {
+        if (interactive) {
+            getElement().setAttribute("interactive", "true");
+        }
+    }
+
+    /**
+     * sets the background color of avatar
+     *
+     * @param color color
+     */
+    public void setBackgroundColor(BackgroundColor color) {
+        getElement().setAttribute("background-color", color.getText());
+    }
+
+    /**
+     * sets image-fit-type attribute of ui5-avatar
+     *
+     * @param type imageFitType
+     */
+    public void setImageFitType(ImageFitType type) {
+        getElement().setAttribute("image-fit-type", type.getText());
+    }
+
+
+    /**
+     * possible values for shape attribute of ui5-avatar
+     */
     @AllArgsConstructor
     @Getter
     public enum Shape {
         CIRCLE("Circle"),
         SQUARE("Square");
 
-        private String attribute;
+        private final String attribute;
     }
 
+    /**
+     * possible values for size attribute of ui5-avatar
+     */
     public enum Size {
         XS,
         S,
@@ -55,4 +118,34 @@ public class Ui5Avatar extends UiComponent {
         XL
     }
 
+    /**
+     * possible values for background-color attribute of ui5-avatar
+     */
+    @AllArgsConstructor
+    @Getter
+    public enum BackgroundColor {
+        ACCENT1("Accent1"),
+        ACCENT2("Accent2"),
+        ACCENT3("Accent3"),
+        ACCENT4("Accent4"),
+        ACCENT5("Accent5"),
+        ACCENT6("Accent6"),
+        ACCENT7("Accent7"),
+        ACCENT8("Accent8"),
+        ACCENT9("Accent9"),
+        ACCENT10("Accent10"),
+        PLACEHOLDER("Placeholder");
+        private final String text;
+    }
+
+    /**
+     * possible values for image-fit-type attribute of ui5-avatar
+     */
+    @AllArgsConstructor
+    @Getter
+    public enum ImageFitType {
+        COVER("Cover"),
+        CONTAIN("Contain");
+        private final String text;
+    }
 }
