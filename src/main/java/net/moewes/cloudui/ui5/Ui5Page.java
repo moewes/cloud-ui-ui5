c5f86bee08f97b34840d957f9dde25f5130df755
Maik Möwes <moewes@users.noreply.github.com>
2021-05-08 13:56:44 +0200

    Ui5 Page component

---
+++ src/main/java/net/moewes/cloudui/ui5/Ui5Page.java
@@ -0,0 +1,11 @@
+package net.moewes.cloudui.ui5;
+
+import net.moewes.cloudui.UiComponent;
+
+// TODO Javadoc
+public class Ui5Page extends UiComponent {
+
+    public Ui5Page() {
+        super("ui5-page");
+    }
+}
