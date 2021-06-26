61d8e567bfb3024f4c85464bbe6d2c874db417de
Maik Möwes <moewes@users.noreply.github.com>
2021-06-26 11:24:52 +0200

    Ui5-table

--- src/main/java/net/moewes/cloudui/ui5/table/Ui5Table.java
+++ src/main/java/net/moewes/cloudui/ui5/table/Ui5Table.java
@@ -1,14 +1,20 @@
 package net.moewes.cloudui.ui5.table;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import net.moewes.cloudui.UiComponent;
 import net.moewes.cloudui.UiElement;
+import net.moewes.cloudui.UiEvent;
+import net.moewes.cloudui.UiEventAttributeMapping;
+import net.moewes.cloudui.UiEventDefinition;
 
 public class Ui5Table<model> extends UiComponent {
 
@@ -21,7 +27,9 @@ public class Ui5Table<model> extends UiComponent {
 
     private List<Column> columns = new ArrayList<>();
 
-    private Collection<model> items;
+    private Function<model, String> idGetter;
+
+    private Collection<model> items = new ArrayList<model>();
 
     public Ui5Table() {
         super("ui5-table");
@@ -46,6 +54,9 @@ public class Ui5Table<model> extends UiComponent {
         items.forEach(item -> {
             Ui5TableRow row = new Ui5TableRow();
             add(row);
+            idGetter().ifPresent(modelStringFunction -> {
+                row.setId(modelStringFunction.apply(item));
+            });
             columns.forEach(column -> {
                 Ui5TableCell cell = new Ui5TableCell();
                 row.add(cell);
@@ -54,5 +65,26 @@ public class Ui5Table<model> extends UiComponent {
         });
 
         return super.render();
+    }
+
+    @Override
+    public void addEventListener(String event, Consumer<UiEvent> function) {
+
+        if ("row-click".equals(event)) {
+            registerEvenrHandler(function, UiEventDefinition.builder().eventName(event)
+                    .attributeMappings(Arrays.asList(
+                            UiEventAttributeMapping.builder().fieldName("row").isHtmlElement(true).build()))
+                    .build());
+        } else {
+            super.addEventListener(event, function);
+        }
+    }
+
+    public void setIdGetter(Function<model, String> getter) {
+        this.idGetter = getter;
+    }
+
+    private Optional<Function<model, String>> idGetter() {
+        return Optional.ofNullable(idGetter);
     }
 }
